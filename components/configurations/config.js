f967df921dfa7c519aaa28e1dc2067caad181771
srireskianita <srireskianita@gmail.com>
2018-07-24 14:23:29 +0700

    edit port config

--- components/configurations/config.js
+++ components/configurations/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    port: 8000,
+    port: process.env.PORT || 8000,
     // dbconn: 'mongodb://localhost:27017',
     dbconn: 'mongodb://admin:admin123@ds247141.mlab.com:47141/bebas',
     myDB: 'bebas'
