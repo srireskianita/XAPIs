ae4ecd5c14a5a4c954ca2cfc0f9893fe83b4a5e5
srireskianita <srireskianita@gmail.com>
2018-08-01 10:39:07 +0700

    commit

--- server.js
+++ server.js
@@ -59,6 +59,11 @@ require('./components/controllers/template.controller')(server, 'test');
 require('./components/controllers/users.controller')(server,'users');
 require('./components/controllers/products.controller')(server,'products');
 require('./components/controllers/categories.controller')(server,'categories');
+require('./components/controllers/template.controller')(server, 'tables');
+require('./components/controllers/template.controller')(server, 'orders');
+require('./components/controllers/template.controller')(server, 'reservations');
+
+
 
 global.config = require('./components/configurations/config');
 
